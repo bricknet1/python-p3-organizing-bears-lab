2db693319d85852453d42cbf9d0028977a25ad98
Nick Johnson <nlj222@gmail.com>
2023-02-27 10:36:11 -0800

    lab complete

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Mr. Chocolate", 3, "F", "Brown", "Nice", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Rowdy", 4, "F", "Brown", "Nice", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Tabitha", 7, "M", "Brown", "Nice", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Sergeant Brown", 1, "F", "Brown", "Nice", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Melissa", 9, "M", "Black", "Nice", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Grinch", 12, "F", "Black", "Nice", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Wendy", 2, "M", "Black", "Nice", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES(NULL, 1, "F", "Black", "Nice", 1);
